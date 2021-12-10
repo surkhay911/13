031781df6e772962da39fb29401fb10adb7062d6
surkhay911 <surkhay77@yandex.ru>
2021-12-10 18:35:48 +0400

    scroll

---
+++ js.js
@@ -0,0 +1,10 @@
+// When the user scrolls the page, execute myFunction 
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  // var winScroll = window.pageYOffset;
+  var winScroll = document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("Bar").style.width = scrolled + "%";
+}
